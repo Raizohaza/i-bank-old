f6eff3ce3e1010af0ac200e83fa61bfbc2600179
Raizohaza <toanphamhcmus@gmail.com>
2022-12-03 23:01:24 +0700

    add cors

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const options = new DocumentBuilder()
     .setTitle('API docs')
     .addTag('users')
